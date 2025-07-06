cbc3f3d1e234b75d9fc81cadec722b29a60d471a
Nicolás Fernández <nicohfernandez21@gmail.com>
2025-07-06 18:35:29 -0300

    .

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// npm install react-router-dom
+//Nico, por favor acordate que vas a tener que programar así: creas cada página y componente en un archivo distinto.
+//Dentro de cada página le metes código HTML e importas el componente a utilizar, previamente programado 
+// y por último en el main, renderizas solo la página
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
